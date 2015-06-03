35eab5cef9301f84db104d8e17e455d3e94e4053
Joao <jpedrofreitas7@gmail.com>
2015-06-03 20:05:40 +0100

    ExData_Plotting1 Project

--- plot3.R
+++ plot3.R
@@ -12,13 +12,12 @@ datetime <- strptime(with(data, sprintf("%s %s", Date, Time)), "%d/%m/%Y %H:%M:%
 Sys.setlocale("LC_TIME", "English")
 
 png("plot3.png")
-with(data, plot(datetime, Sub_metering_1, type = "l",ylab = "Energy sub metering", xlab=""))
-# par(new=TRUE)
-with(data, lines(datetime, Sub_metering_2, type = "l", col = "red"))
-# par(new=TRUE)
-with(data, lines(datetime, Sub_metering_3, type = "l", col = "blue"))
-legend(x = "topright",
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       col=c("black","red", "blue"), lty = 1)
+with(data,{plot(datetime, Sub_metering_1, type = "l",ylab = "Energy sub metering", xlab="")
+     lines(datetime, Sub_metering_2, type = "l", col = "red")
+     lines(datetime, Sub_metering_3, type = "l", col = "blue")
+     legend(x = "topright",
+            legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+            col=c("black","red", "blue"), lty = 1, bty = "n")
+})
 dev.off()
 
--- plot4.R
+++ plot4.R
@@ -13,29 +13,20 @@ Sys.setlocale("LC_TIME", "English")
 
 png("plot4.png")
 par(mfrow=c(2,2))
-# ==
-with(data,plot(datetime, Global_active_power, type = "l",
+with(data,{plot(datetime, Global_active_power, type = "l",
                main = "",
                ylab = "Global Active Power (kilowatts)",
-               xlab = ""))
-# ==
-with(data,plot(datetime, Voltage, type = "l",
-               main = "",
-               ylab = "Voltage"))
-# ==
-with(data, plot(datetime, Sub_metering_1, type = "l",ylab = "Energy sub metering", xlab=""))
-# par(new=TRUE)
-with(data, lines(datetime, Sub_metering_2, type = "l", col = "red"))
-# par(new=TRUE)
-with(data, lines(datetime, Sub_metering_3, type = "l", col = "blue"))
-legend(x = "topright",
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       col=c("black","red", "blue"), lty = 1)
-# ==
-with(data,plot(datetime, Global_reactive_power, type = "l",
-               main = ""))
+               xlab = "")
+           plot(datetime, Voltage, type = "l",
+                              main = "",
+                              ylab = "Voltage")
+           plot(datetime, Sub_metering_1, type = "l",ylab = "Energy sub metering", xlab="")
+           lines(datetime, Sub_metering_2, type = "l", col = "red")
+           lines(datetime, Sub_metering_3, type = "l", col = "blue")
+           legend(x = "topright",
+                  legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+                  col=c("black","red", "blue"), lty = 1, bty = "n")
+           plot(datetime, Global_reactive_power, type = "l",
+                main = "")
+           })
 dev.off()
-
-
-par(mfrow=c(2,2))
-
